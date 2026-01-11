b9f0e7bb9f0225a5e97a0198ed47207112eeeafd
bikashmaz <148486924+bikashmaz@users.noreply.github.com>
2026-01-11 21:35:02 +0530

    Github Actions and Mochawesome report added

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
