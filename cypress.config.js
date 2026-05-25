58a45a02ad15a803df520ca8bd806a4b87f224e2
Bikash Mazumder <148486924+bikashmaz@users.noreply.github.com>
2026-05-25 16:13:54 +0530

    fix: increase Cypress timeouts to handle network delays

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    requestTimeout: 15000,
+    responseTimeout: 15000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
