2f704b1dee17caef37f65cfbc67722b51cdb2240
Bikash Mazumder <148486924+bikashmaz@users.noreply.github.com>
2026-05-25 16:06:22 +0530

    Refactor logout test to use contains for Logout link

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -10,10 +10,10 @@ describe('OrangeHRM Logout Tests', () => {
     cy.contains('Dashboard').should('be.visible');
   });
 
-  it('Logout Functionality', () => {
+ it('Logout Functionality', () => {
   cy.get('i.oxd-userdropdown-icon').should('be.visible').click();
-  cy.get('a.oxd-userdropdown-link').contains('Logout').should('be.visible').click();
+  cy.contains('a.oxd-userdropdown-link', 'Logout').should('be.visible').click();
   cy.get('img[alt="company-branding"]').should('be.visible');
-  });
+});
 
 });
