a05e30f9d929b08099db3fc104e55e0e8bfdd06b
Bikash Mazumder <148486924+bikashmaz@users.noreply.github.com>
2026-05-27 10:54:09 +0530

    Fix logout test timeout: increase timeout for company-branding element detection

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -3,9 +3,10 @@ describe('OrangeHRM Logout Tests', () => {
   beforeEach(() => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
       retryOnNetworkFailure: true,
-      retryOnStatusCode: [502, 503, 504]
+      retryOnStatusCode: [502, 503, 504],
+      timeout: 10000
     });
-    cy.get('img[alt="company-branding"]').should('be.visible');
+    cy.get('img[alt="company-branding"]', { timeout: 8000 }).should('be.visible');
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('admin123');
     cy.contains('button', 'Login').click();
@@ -16,7 +17,7 @@ describe('OrangeHRM Logout Tests', () => {
   it('Logout Functionality', () => {
     cy.get('i.oxd-userdropdown-icon').should('be.visible').click();
     cy.contains('a.oxd-userdropdown-link', 'Logout').should('be.visible').click();
-    cy.get('img[alt="company-branding"]').should('be.visible');
+    cy.get('img[alt="company-branding"]', { timeout: 8000 }).should('be.visible');
   });
 
 });
